616b4757308ddf00a39bc80fd722c7f87ffc6cc2
k120v4 <k120v4@protonmail.com>
2023-05-30 17:06:48 -0700

    picture and content

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useRef } from "react";
 import { ethers } from "ethers";
 import "./style.css";
 import lostpic from './2.jpg';
+import computer from './computer.png';
 
 const contractABI = require("./Island.json");
 const YOUR_CONTRACT_ADDRESS = "0x6f1b3eB120dd6dE6ADdFd54dd47AB62e3f3C3812";
@@ -28,7 +29,7 @@ export default function App() {
   //functions for the buttons
   let enterTheCode = async () => {
     console.log(query)
-    if (query == 4815162342) {
+    if (query == '4 8 15 16 23 42') {
       console.log('correct');
       await getContract().enterTheCode({gasLimit: 50000});
     }
@@ -133,16 +134,10 @@ export default function App() {
               <p class="blank"> ..</p>
               {!systemFailure && <h1 class="title">{countdown} </h1>}
               {systemFailure && <h1 class="redtitle">XX:XX:XX </h1>}
-              <h3 class="text"> Every 1080 minutes, the button must be pushed. From the moment the alarm sounds, you will have four minutes to enter the code into the microcomputer processor.
-
-Either you or your partners must input the code. It is highly recommended that you and your partners take alternating shifts. In this manner you will all stay fresh and alert.
-
-Congratulations! Until your replacements arrive, the future of the project is in your hands. On behalf of the Degroots, Alvar Hanso and all of us at the Dharma Initiative, thank you. Namaste. And good luck.</h3>
-
-              
-              
+              <img src={computer} width="50%"/>
+              <p></p>
               <label class="descrip" >
-                Type in the numbers (no spaces or commas): 
+                Type in the numbers with spaces: 
                 <input value={query} onChange={(e)=>setQuery(e.target.value)} type="text"></input>
               </label>
               <button onClick={enterTheCode} class="button">
@@ -154,24 +149,41 @@ Congratulations! Until your replacements arrive, the future of the project is in
 
               <h3 class="faqtitle"> FAQ </h3>
               <p class="faqb"> What is this? </p>
-              <p class="faq"> $LOST is a memecoin based on the TV Show Lost. No presale, no tax, LP locked, no owner, no fuss. </p>
+              <p class="faq"> $LOST is a memecoin inspired by the TV series Lost. No presale, no tax, LP locked, no owner, no fuss. 
+              Every 1080 minutes (18hrs), the button must be pushed. It is highly recommended that you and your partners take alternating shifts. 
+              In this manner you will all stay fresh and alert.</p>
+
               <p class="faqb"> What are the numbers? </p>
               <p class="faq"> 4 8 15 16 23 42</p>
+             
               <p class="faqb"> What happens if the timer runs out? </p>
-              <p class="faq"> System Failure. Occupants of the Swan station followed a protocol in which they typed a sequence into a computer every 
-              108 minutes. Typing these numbers and pressing "execute" (a.k.a. pushing the button) on the keyboard
-              averts worldwide catastrophe. Worldwide catastrophe in our case means number go down. The Island holds 45% of the tokens 
-              and will flood the LP with 10% of its tokens on each System Failure. The timer has also been lengthened to 1080 minutes (18 hrs).</p>
+              <p class="faq"> A catastrophic System Failure event aka number go down. The Island holds 45% of the tokens 
+              and will flood the LP with 10% of its tokens on each System Failure. </p>
+             
               <p class="faqb"> Tokenomics? </p>
               <p class="faq"> 1,000,000,000,000 token supply, 10% CEX/marketing, 45% LP, 45% island (basically burnt if everyone keeps pressing the button!)</p>
+             
               <p class="faqb"> Contracts and Addresses </p>
-              <p class="faq"> <a href="http://">Token</a>, <a href="http://">Island</a>, <a href="http://">Marketing wallet </a></p>
+             
+              <p class="faq"> Mainnet: <a href="http://"> Token</a>, 
+                                      <a href="http://"> Island</a>, 
+                                      <a href="http://"> Marketing wallet </a></p>
+             
               <p class="faqb"> Links </p>
-              <p class="faq"> <a href="http://">Twitter</a>, <a href="http://">Discord</a></p>
+              <p class="faq"> <a href="https://twitter.com/lostcoin_eth"> Twitter</a>, 
+                              <a href="http://"> Discord</a>, 
+                              <a href="http://"> Dextools</a>, 
+                              <a href="http://"> CMC</a>, 
+                              <a href="http://"> Uniswap</a> </p>
+             
+              <p class="faqb"> Any last words? </p>
+              <p class="faq"> Congratulations! Until your replacements arrive, the future of the project is in your hands. 
+                            On behalf of the Degroots, Alvar Hanso and all of us at the Dharma Initiative, thank you. Namaste. And good luck. </p>
+                              
              
               <p class="descrip">===============================================================================</p>
               <img src={lostpic} width="30%"/>
-              
+              <p class="blank"> ..</p>           
              
 
               
